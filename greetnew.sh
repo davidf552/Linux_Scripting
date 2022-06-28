1d794140f3d8191bf91613eb1f10a4394d789b2f
davidf552 <103103116+davidf552@users.noreply.github.com>
2022-06-28 02:23:35 -0400

    Add files via upload

---
+++ greetnew.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+#We start asking for your first name
+echo -n "Insert your first name:"
+read fname
+
+#Then we continue by asking your last name
+echo -n "Insert your last name:"
+read lname
+
+echo "Hello $fname $lname"
